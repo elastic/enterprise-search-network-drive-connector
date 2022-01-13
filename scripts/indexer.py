a799fb8efbe7647f84bca9d86df7aa4689916fa2
praveen-elastic <praveen.kukreja@elastic.co>
2022-01-13 16:45:03 +0530

    add indexing rule implementation

--- scripts/indexer.py
+++ scripts/indexer.py
@@ -3,18 +3,215 @@
 # 2.0; you may not use this file except in compliance with the Elastic License
 # 2.0.
 
-class Indexer:
-    """ This class is responsible for indexing the documents from Network Drive to the Enterprise Search.
-    """
+import multiprocessing
+from datetime import datetime
+import copy
+import json
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import src.logger_manager as log
+from elastic_enterprise_search import WorkplaceSearch
+from src.network_drive_client import NetworkDrive
+from src.configuration import Configuration
+from src.constant import DATETIME_FORMAT, IDS_PATH, DOCUMENT_SIZE
+from src.checkpointing import Checkpoint
+from src.files import Files
+from src.base_class import BaseClass
+import time
 
-    def indexing(self):
-        """ This method fetches all the files and ingest them into Enterprise Search.
+
+logger = log.setup_logging("network_drive_connector_index")
+
+
+class Indexer(BaseClass):
+
+    def __init__(self, start_time, end_time, checkpoint):
+        logger.info("Initializing the Indexing class")
+        self.logger = logger
+        self.is_error = False
+        BaseClass.__init__(self, logger=logger)
+        self.start_time = start_time
+        self.end_time = end_time
+        self.checkpoint = checkpoint
+        self.network_drive_client = NetworkDrive(logger)
+        self.ws_client = WorkplaceSearch(self.ws_host, http_auth=self.ws_token)
+
+    def index_document(self, document):
+        """ This method indexes the documents to the workplace.
+            :param document: list of documents to be indexed
         """
-        pass
+        try:
+            if document:
+                total_documents_indexed = 0
+                document_list = [document[i * DOCUMENT_SIZE:(i + 1) * DOCUMENT_SIZE] for i in range((len(document) + DOCUMENT_SIZE - 1) // DOCUMENT_SIZE)]
+                for chunk in document_list:
+                    response = self.ws_client.index_documents(
+                        content_source_id=self.ws_source,
+                        documents=chunk
+                    )
+                    for each in response['results']:
+                        if not each['errors']:
+                            total_documents_indexed += 1
+                        else:
+                            logger.error("Unable to index the document with id: %s Error %s" % (each['id'], each['errors']))
+                logger.info("Successfully indexed %s to the workplace out of %s" % (
+                    total_documents_indexed, len(response['results'])))
+        except Exception as exception:
+            logger.exception("Error while indexing the files. Error: %s"
+                             % (exception)
+                             )
+            self.is_error = True
+
+    def indexing(self, drive, ids, storage, is_error_shared):
+        """This method fetches all the objects from Network Drive server and
+            ingests them into the workplace search
+            :param drive: drive name
+            :param ids: id drive of the all the objects
+            :param storage: temporary storage for storing all the documents
+            :param is_error_shared: list of all the is_error values
+        """
+        time_range = {'start_time': self.start_time, 'end_time': self.end_time}
+        connection = self.network_drive_client.connect()
+        if connection:
+            files = Files(self.logger)
+            document = files.fetch_files(connection, time_range)
+            for doc in document:
+                ids["files"].update({doc["id"]: doc['url']})
+            self.index_document(document)
+            logger.info(
+                "Completed fetching all the objects for drive: %s"
+                % (drive)
+            )
+            connection.close()
+            storage.update(ids)
+        else:
+            self.is_error = True
+        is_error_shared.append(self.is_error)
 
 
-def start():
-    """ Runs the indexing logic regularly after a given interval
-        or puts the connector to sleep
+def datetime_partitioning(start_time, end_time, processes):
+    """ Divides the timerange in equal partitions by number of processors
+        :param start_time: start time of the interval
+        :param end_time: end time of the interval
+        :param processes: number of processors the device have
     """
-    pass
+    start_time = datetime.strptime(start_time, DATETIME_FORMAT)
+    end_time = datetime.strptime(end_time, DATETIME_FORMAT)
+
+    diff = (end_time - start_time) / processes
+    for idx in range(processes):
+        yield (start_time + diff * idx)
+    yield end_time
+
+
+def init_multiprocessing(start_time, end_time, drive, ids, storage, is_error_shared, checkpoint):
+    """This method initializes the IndexUpdate class and kicks-off the multiprocessing. This is a wrapper method added to fix the pickling issue while using multiprocessing in Windows
+            :param start_time: start time of the indexing
+            :param end_time: end time of the indexing
+            :param drive: drive name
+            :param ids: id drive of the all the objects
+            :param storage: temporary storage for storing all the documents
+            :param is_error_shared: list of all the is_error values
+            :checkpoint: checkpoint details
+        """
+    indexer = Indexer(start_time, end_time, checkpoint)
+    indexer.indexing(drive, ids, storage, is_error_shared)
+
+
+def start(indexing_type):
+    """Runs the indexing logic regularly after a given interval
+        or puts the connector to sleep
+        :param indexing_type: The type of the indexing i.e. Incremental Sync or Full sync
+    """
+    logger.info("Starting the indexing..")
+    config = Configuration(logger)
+    data = config.configurations
+    is_error_shared = multiprocessing.Manager().list()
+    while True:
+        current_time = (datetime.utcnow()).strftime(DATETIME_FORMAT)
+        ids_collection = {"global_keys": {}}
+        storage_with_collection = {"global_keys": {}, "delete_keys": {}}
+
+        if (os.path.exists(IDS_PATH) and os.path.getsize(IDS_PATH) > 0):
+            with open(IDS_PATH) as ids_store:
+                try:
+                    ids_collection = json.load(ids_store)
+                except ValueError as exception:
+                    logger.exception(
+                        "Error while parsing the json file of the ids store from path: %s. Error: %s"
+                        % (IDS_PATH, exception)
+                    )
+
+        storage_with_collection["delete_keys"] = copy.deepcopy(ids_collection.get("global_keys"))
+
+        drive = data.get("network_drive.server_name")
+        storage = multiprocessing.Manager().dict({"files": {}})
+        logger.info(
+            "Starting the data fetching for drive: %s"
+            % (drive)
+        )
+        check = Checkpoint(logger)
+
+        worker_process = data.get("worker_process")
+        if indexing_type == "incremental":
+            start_time, end_time = check.get_checkpoint(
+                current_time, drive)
+        else:
+            start_time = data.get("start_time")
+            end_time = current_time
+
+        # partitioning the drive timeframe in equal parts by worker processes
+        partitions = list(datetime_partitioning(
+            start_time, end_time, worker_process))
+
+        datelist = []
+        for sub in partitions:
+            datelist.append(sub.strftime(DATETIME_FORMAT))
+
+        jobs = []
+        if not ids_collection["global_keys"].get(drive):
+            ids_collection["global_keys"][drive] = {
+                "files": {}}
+
+        for num in range(0, worker_process):
+            start_time_partition = datelist[num]
+            end_time_partition = datelist[num + 1]
+            logger.info(
+                "Successfully fetched the checkpoint details: start_time: %s and end_time: %s, calling the indexing"
+                % (start_time_partition, end_time_partition)
+            )
+
+            process = multiprocessing.Process(target=init_multiprocessing, args=(start_time_partition, end_time_partition, drive, ids_collection["global_keys"][drive], storage, is_error_shared, check))
+            jobs.append(process)
+
+        for job in jobs:
+            job.start()
+        for job in jobs:
+            job.join()
+        storage_with_collection["global_keys"][drive] = storage.copy()
+        logger.info(
+            "Saving the checkpoint for the drive: %s" % (drive)
+        )
+        if True in is_error_shared:
+            check.set_checkpoint(start_time, indexing_type, drive)
+        else:
+            check.set_checkpoint(end_time, indexing_type, drive)
+
+        with open(IDS_PATH, "w") as f:
+            try:
+                json.dump(storage_with_collection, f, indent=4)
+            except ValueError as exception:
+                logger.warn(
+                    'Error while adding ids to json file. Error: %s' % (exception))
+        if indexing_type == "incremental":
+            interval = data.get("indexing_interval")
+        else:
+            interval = data.get("full_sync_interval")
+        # TODO: need to use schedule instead of time.sleep
+        logger.info("Sleeping..")
+        time.sleep(interval * 60)
+
+
+if __name__ == "__main__":
+    start("incremental")
--- src/indexing_rule.py
+++ src/indexing_rule.py
@@ -2,3 +2,68 @@
 # or more contributor license agreements. Licensed under the Elastic License
 # 2.0; you may not use this file except in compliance with the Elastic License
 # 2.0.
+
+import re
+from wcmatch import glob
+
+
+class IndexingRules:
+    def filter_size(self, file_details, symbol, pattern):
+        """This method is used to find if the file size is matching with the pattern
+            :param file_details: dictionary containing file properties
+            :param symbol: >,<,>=,<=,!,!=,=,== symbol
+            :param pattern: numeric part of pattern as a string
+            :returns: True or False denoting whether the file size is according to the pattern
+        """
+        file_size = file_details['file_size']
+        operation = {
+            '>': file_size > int(pattern),
+            '>=': file_size >= int(pattern),
+            '<': file_size < int(pattern),
+            '<=': file_size <= int(pattern),
+            '!': file_size != int(pattern),
+            '!=': file_size != int(pattern),
+            '=': file_size == int(pattern),
+            '==': file_size == int(pattern),
+        }
+        return operation.get(symbol)
+
+    def apply_rules(self, file_details, include, exclude):
+        """This method is used to check if the current file is following the indexing rule or not
+            :param file_details: dictionary containing file properties
+            :param include: include pattern provided for matching
+            :param exclude: exlcude pattern for matching
+            :returns: True or False denoting if the file is to following the indexing rule or not
+        """
+        result = True
+        if include:
+            for filtertype, pattern in include.items():
+                result = self.filter_pattern(filtertype, pattern, file_details, 'include')
+                if result is False:
+                    return False
+        if exclude:
+            for filtertype, pattern in exclude.items():
+                for value in (pattern or []):
+                    if include and (value in (include.get(filtertype) or [])):
+                        pattern.remove(value)
+                result = self.filter_pattern(filtertype, pattern, file_details, 'exclude')
+                if result is False:
+                    return False
+        return result
+
+    def filter_pattern(self, filtertype, pattern, file_details, pattern_type):
+        """This method is used to connect with network drive.
+            :filtertype: denotes the type of filter used: size/path_template
+            :param pattern: include/ exclude pattern provided for matching
+            :param file_details: dictionary containing file properties
+            :param pattern_type: include/exlcude
+        """
+        for value in (pattern or []):
+            if filtertype == 'size':
+                initial = re.match('[>,<,=,!]=?', value)
+                result = self.filter_size(file_details, initial[0], re.findall("[0-9]+", value)[0])
+            else:
+                result = glob.globmatch(file_details['file_path'], value, flags=glob.GLOBSTAR)
+            if (pattern_type == 'include' and not(result)) or (pattern_type == 'exclude' and result):
+                return False
+        return True
