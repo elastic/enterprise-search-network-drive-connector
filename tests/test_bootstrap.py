4eba37ce596dd30b4129e2e86badd2f4e49dca35
praveen-elastic <praveen.kukreja@elastic.co>
2022-06-03 12:49:13 +0530

    add testcose with user args in bootstrap

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -38,3 +38,20 @@ def test_execute(caplog):
     )
     bootstrap_obj.execute()
     assert "Created ContentSource with ID 1234." in caplog.text
+
+
+def test_execute_with_user_argument(caplog):    
+    """Test execute method in Bootstrap file creates a content source using user argument in the Enterprise Search."""
+    args = argparse.Namespace()
+    args.name = "dummy"
+    args.user = "user1"
+    args.password = "user123"
+    args.config_file = CONFIG_FILE
+    caplog.set_level("INFO")
+    mock_response = {"id": "1234"}
+    bootstrap_obj = BootstrapCommand(args)
+    bootstrap_obj.workplace_search_custom_client.workplace_search_client.create_content_source = Mock(
+        return_value=mock_response
+    )
+    bootstrap_obj.execute()
+    assert "Created ContentSource with ID 1234." in caplog.text
